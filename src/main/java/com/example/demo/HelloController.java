7df3e4edf7956ee59312f11885a400858eb82b4a
huangli <1833002417@qq.com>
2018-02-28 14:52:56 +0800

    add images

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by huangli on 2018/2/27.
  */
+// 访问地址： http://localhost:8080/hello
 @RestController
-public class HellowController {
+public class HelloController {
     @RequestMapping("hello")
     public String say(){
         return "hello Java!";
